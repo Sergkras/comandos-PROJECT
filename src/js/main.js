cbfaef4c39567a915d18e375d35a011fc6008096
Sergkras <seakras@ukr.net>
2021-05-17 21:55:12 +0300

    пятница

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import './services/start-page';
+import { showAlert, showError } from './components/pnotify';
 import createsDownloadList from './services/nev-render-list';
 import EventsApiService from './services/services';
 import eventsListTpl from '../templates/card-list.hbs';
@@ -12,47 +13,52 @@ const eventsApiService = new EventsApiService();
 refs.searchInput.addEventListener('submit', onSearch);
 
 async function onSearch(e) {
-   
+
    try {
      e.preventDefault();
-      
+
      eventsApiService.query = e.currentTarget.elements.query.value;
-      
+
      if (eventsApiService.query === '' || !eventsApiService.query.trim()) {
       //   тут треба вивести помилку пошуку
-      return
+       return
      }
- 
+
      await eventsApiService.resetPage();
 
      await clearEvents();
- 
-     const events = await eventsApiService.fetchEvent({})
-     console.log(events);
 
-     if(events.length === 0){
+     const events = await eventsApiService.fetchEvent({})
+
+     console.log(events.[0]);
+
+     if(newFetchEventList.length === 0) {
+       fetchError(error)
       //   тут треба вивести помилку пошуку
-      return
+       return
     }
 
     //  await renderEventList(events)
      const newFetchEventList = createsDownloadList(events);
      await renderEventList(newFetchEventList);
 
-     } catch (err){
-        // console.log(err);
-      //   тут треба вивести помилку запиту fetch
-      
-     }
-     
- }
+   }
+      catch (err) {
 
+  //       console.log(err);
+      //   тут треба вивести помилку запиту fetch
+     }
+}
+
+function fetchError(error) {
+    showError('This event was not found')
+};
 
  // додав, щоб перевырити роботу пошуку, хто відповідає за цей функціонал замінете...
  function renderEventList(events){
    eventsMarkup(events)
    eventsApiService.incrementPage()
-}
+};
 
 function eventsMarkup(events) {
    refs.cardEvent.insertAdjacentHTML('beforeend', eventsListTpl(events));
--- src/js/services/services.js
+++ src/js/services/services.js
@@ -15,7 +15,7 @@ export default class EventsApiService {
     }
 
     const result = await rawResult.json();
-    
+
     // зразу повертаємо масив подій
     return result._embedded.events;
   }
